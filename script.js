89f2c97750d140e4e2222149bc0566cbb9d2161f
MirkhakimAzizov <azizov.mirkhakimjon@gmail.com>
2023-03-08 14:10:29 +0500

    calculator script jc

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ console.log(elBtn);
 
 let numberOne = "";
 let numberTwo = "";
+let number = "";
 
 elBtn[12].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "1";
         numberTwo = numberTwo + "1";
-        value.innerHTML = numberOne;
+        number = number + "1";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -20,7 +22,8 @@ elBtn[13].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "2";
         numberTwo = numberTwo + "2";
-        value.innerHTML = numberOne;
+        number = number + "2";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -30,7 +33,8 @@ elBtn[14].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "3";
         numberTwo = numberTwo + "3";
-        value.innerHTML = numberOne;
+        number = number + "3";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -40,7 +44,8 @@ elBtn[8].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "4";
         numberTwo = numberTwo + "4";
-        value.innerHTML = numberOne;
+        number = number + "4";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -50,7 +55,8 @@ elBtn[9].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "5";
         numberTwo = numberTwo + "5";
-        value.innerHTML = numberOne;
+        number = number + "5";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -60,7 +66,8 @@ elBtn[10].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "6";
         numberTwo = numberTwo + "6";
-        value.innerHTML = numberOne;
+        number = number + "6";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -71,7 +78,8 @@ elBtn[4].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "7";
         numberTwo = numberTwo + "7";
-        value.innerHTML = numberOne;
+        number = number + "7";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -81,7 +89,8 @@ elBtn[5].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "8";
         numberTwo = numberTwo + "8";
-        value.innerHTML = numberOne;
+        number = number + "8";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -91,7 +100,8 @@ elBtn[6].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "9";
         numberTwo = numberTwo + "9";
-        value.innerHTML = numberOne;
+        number = number + "9";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -101,7 +111,8 @@ elBtn[16].addEventListener("click", (evt) => {
     if (numberOne.length < 15) {
         numberOne = numberOne + "0";
         numberTwo = numberTwo + "0";
-        value.innerHTML = numberOne;
+        number = number + "0";
+        value.innerHTML = number;
     } else {
         error = "the maximum number has been reached";
     }
@@ -113,8 +124,11 @@ let math;
 let resalt;
 
 elBtn[7].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
+    number = "";
     numberTwo = "";
     math = "";
     math = "/";
@@ -122,56 +136,74 @@ elBtn[7].addEventListener("click", (evt) => {
 });
 
 elBtn[11].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
     numberTwo = "";
     math = "";
     math = "*";
+    number = "";
     value.innerHTML = "";
 });
 
 elBtn[15].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
     numberTwo = "";
     math = "";
     math = "-";
+    number = "";
     value.innerHTML = "";
 });
 
 elBtn[19].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
     numberTwo = "";
     math = "";
     math = "+";
+    number = "";
     value.innerHTML = "";
 });
 
 elBtn[0].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
     numberTwo = "";
     math = "";
     math = "pow";
+    number = "";
     value.innerHTML = "";
 });
 
 elBtn[1].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
     numberTwo = "";
     math = "";
     math = "sqrt";
+    number = "";
     value.innerHTML = "";
 });
 
 elBtn[2].addEventListener("click", (evt) => {
-    numOne = numberOne;
+    if (numberOne != "") {
+        numOne = numberOne;
+    }
     numberOne = "";
     numberTwo = "";
     math = "";
     math = "%";
+    number = "";
     value.innerHTML = `${numOne}%`;
 });
 
@@ -179,9 +211,10 @@ elBtn[3].addEventListener("click", (evt) => {
     numberOne = "";
     numberTwo = "";
     math = "";
+    number = "";
     value.innerHTML = "";
 });
-
+let i = 0;
 elBtn[18].addEventListener("click", (evt) => {
     numTwo = numberTwo;
     numberOne = "";
